93e4ad979301ad1f03be1315ffcde43973a7e64a
kubasipika <kubasipika@192.168.1.104>
2015-11-14 21:47:50 +0100

    Simple textures.

--- FusionEngine/src/engineTester/MainGameLoop.java
+++ FusionEngine/src/engineTester/MainGameLoop.java
@@ -6,40 +6,44 @@ import org.lwjgl.opengl.GL11;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
+import models.TexturedModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
-		
+
 		DisplayManager.createDisplay();
-	
+
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
-		StaticShader shader =new StaticShader();
-		
-		float[] vertices = {
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f
+		StaticShader shader = new StaticShader();
+
+		float[] vertices = { -0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f };
+
+		int[] indices = { 0, 1, 3, 3, 1, 2 };
+
+		float[] textureCoords=
+		{
+			0,0,
+			0,1,
+			1,1,
+			1,0
 		};
 		
-		int[] indices={0,1,3,
-						3,1,2};
-		
-	RawModel model = loader.loadToVAO(vertices,indices);
-	 	
-		
-		while(!Display.isCloseRequested())
-		{
+		RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
+
+		while (!Display.isCloseRequested()) {
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
-			//logika
-			//renderer.render(model);
+			// logika
+			// renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
 		shader.cleanUp();
