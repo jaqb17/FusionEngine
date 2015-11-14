ba35e63db55f190cb1159c94ca6a1357bd73f7e5
kubasipika <kubasipika@192.168.1.104>
2015-11-14 19:34:30 +0100

    Added shaders!

--- FusionEngine/src/engineTester/MainGameLoop.java
+++ FusionEngine/src/engineTester/MainGameLoop.java
@@ -7,37 +7,34 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
-		
+
 		DisplayManager.createDisplay();
-	
+
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
-		
-		float[] vertices = {
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f
-		};
-		
-		int[] indices={0,1,3,
-						3,1,2};
-		
-	RawModel model = loader.loadToVAO(vertices,indices);
-	 	
-		
-		while(!Display.isCloseRequested())
-		{
+		StaticShader shader = new StaticShader();
+
+		float[] vertices = { -0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f };
+
+		int[] indices = { 0, 1, 3, 3, 1, 2 };
+
+		RawModel model = loader.loadToVAO(vertices, indices);
+
+		while (!Display.isCloseRequested()) {
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
-			//logika
-			//renderer.render(model);
+			shader.stop();
+			// logika
+			// renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
