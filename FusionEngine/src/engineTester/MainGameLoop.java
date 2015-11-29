93a1ad798e510aa9003341c3c1f3f4e7b8f0dac1
Komputer <Komputer@DESKTOP-5G9R8AU>
2015-11-29 19:23:48 +0100

    fucked up textures but some obj works

--- FusionEngine/src/engineTester/MainGameLoop.java
+++ FusionEngine/src/engineTester/MainGameLoop.java
@@ -8,6 +8,7 @@ import Entities.Camera;
 import Entities.Entity;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.Renderer;
@@ -18,126 +19,44 @@ import textures.ModelTexture;
 
 public class MainGameLoop {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		DisplayManager.createDisplay();
+	DisplayManager.createDisplay();
 
-		
-		Loader loader = new Loader();
-		
-		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
-		
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
-		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
+	Loader loader = new Loader();
 
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
+	StaticShader shader = new StaticShader();
+	Renderer renderer = new Renderer(shader);
 
-		};
-		
-		RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
-		TexturedModel staticModel = new TexturedModel(model, texture);
-		
-		Entity entity= new Entity(staticModel,new Vector3f(0.5f,0,-1f),0,0,0,0.5f); 
-		
-		Entity entity2= new Entity(staticModel,new Vector3f(-0.5f,0,-1f),0,0,0,0.5f); 
-		
-		Camera camera=new Camera();
+	RawModel model = OBJLoader.loadObjModel("dragon", loader);
+	ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+	TexturedModel staticModel = new TexturedModel(model, texture);
 
-		while (!Display.isCloseRequested()) {
-			entity.increasePosition(0, 0, -0.002f);
-			entity.increaseRotation(1, 1, 1);
-			entity2.increaseRotation(-1, -1, -1);
-			entity2.increasePosition(0, 0, -0.002f);
-			camera.move();
-			renderer.prepare();
-			shader.start();
-			shader.loadViewMatrix(camera);
-			renderer.render(entity,shader);
-			renderer.render(entity2, shader);
-			shader.stop();
-			// logika
-			// renderer.render(model);
-			DisplayManager.updateDisplay();
-			
-		}
-		shader.cleanUp();
-		loader.cleanUp();
-		DisplayManager.closeDisplay();
+	Entity entity = new Entity(staticModel, new Vector3f(0, 0, -15f), 0, 0, 0, 0.5f);
+
+	
+
+	Camera camera = new Camera();
+
+	while (!Display.isCloseRequested()) {
+	    entity.increaseRotation(0, 1, 0);
+	    entity.increasePosition(0, 0,-0.002f );
+	    camera.move();
+	    renderer.prepare();
+	    shader.start();
+	    shader.loadViewMatrix(camera);
+	    renderer.render(entity, shader);
+	   
+	    shader.stop();
+	    // logika
+	    // renderer.render(model);
+	    DisplayManager.updateDisplay();
 
 	}
+	shader.cleanUp();
+	loader.cleanUp();
+	DisplayManager.closeDisplay();
+
+    }
 
 }
